fb74654b67ff2818e9221f2af3107b93f36af36c
frobulochkina <konyakova.elena@gmail.com>
2022-02-17 17:38:10 +0300

    fixed DummyDic

--- src/main/java/ru/job4j/oop/DummyDic.java
+++ src/main/java/ru/job4j/oop/DummyDic.java
@@ -3,7 +3,6 @@ package ru.job4j.oop;
 public class DummyDic {
 
     public String engToRus(String eng) {
-        String answer = "Неизвестное слово. ";
-        return answer + eng;
+        return "Неизвестное слово. " + eng;
     }
 }
