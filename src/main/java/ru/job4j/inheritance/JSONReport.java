9a76fa8b8687ee5ed74f33b001ba3c211c19bca8
frobulochkina <konyakova.elena@gmail.com>
2022-02-18 14:32:53 +0300

    fixed JSONReport

--- src/main/java/ru/job4j/inheritance/JSONReport.java
+++ src/main/java/ru/job4j/inheritance/JSONReport.java
@@ -3,6 +3,6 @@ package ru.job4j.inheritance;
 public class JSONReport extends TextReport {
     @Override
     public String generate(String name, String body) {
-        return "{\n" + "\t" + "\"name\" : \"" + name  + "\",\n" + "\t" + "\"body\" : \"" + body + "\"\n}";
+        return "{"+ System.lineSeparator() + "\t" + "\"name\" : \"" + name  + "\"," + System.lineSeparator() + "\t" + "\"body\" : \"" + body + "\"" +System.lineSeparator() +"}" ;
     }
 }
