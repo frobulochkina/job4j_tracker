723a17ef44a5f0ef0e9d4d8c2bd9a2cf9f97b0b3
frobulochkina <konyakova.elena@gmail.com>
2022-02-19 14:16:33 +0300

    fixed ShopDrop

--- src/main/java/ru/job4j/pojo/ShopDrop.java
+++ src/main/java/ru/job4j/pojo/ShopDrop.java
@@ -4,12 +4,9 @@ public class ShopDrop {
 
     public static Product[] delete(Product[] products, int index) {
         for (int i = index; i < products.length; i++) {
-            if (i == products.length - 1) {
-                products[i] = null;
-                break;
-            }
             products[i] = products[i + 1];
         }
+        products[products.length - 1] = null;
         return products;
     }
 
