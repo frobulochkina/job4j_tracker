376e41bcbdf28dfea32cc04231e399490b9732fb
frobulochkina <konyakova.elena@gmail.com>
2022-02-19 18:14:27 +0300

    add io/Matches

---
+++ src/main/java/ru/job4j/io/Matches.java
@@ -0,0 +1,34 @@
+package ru.job4j.io;
+
+import java.util.Scanner;
+
+public class Matches {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Игра 11.");
+        boolean turn = true;
+        int count = 11;
+        while (count > 0) {
+            String player = turn ? "Первый игрок" : "Второй игрок";
+            System.out.println(player + " введите число от 1 до " + (count > 3 ? 3 : count) + ": ");
+            String str = input.nextLine();
+            boolean lineNotEmpty = str.length() != 0;
+            if (lineNotEmpty) {
+                int matches = Integer.parseInt(str);
+                if (matches > 0 && matches <= 3 && matches <= count) {
+                    turn = !turn;
+                    System.out.println("Осталось: " + (count -= matches) + " спичек.");
+                } else {
+                    System.out.println("Ошибка! Введите число от 1 до " + (count > 3 ? 3 : count) + ": ");
+                }
+            } else {
+                System.out.println("Осталось: " + count + " спичек.");
+            }
+        }
+        if (!turn) {
+            System.out.println("Выиграл первый игрок");
+        } else {
+            System.out.println("Выиграл второй игрок");
+        }
+    }
+}
