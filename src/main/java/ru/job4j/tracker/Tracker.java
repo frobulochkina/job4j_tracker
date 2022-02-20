bb5d978bd68c66f539ead962d6f3d4a56679ea6b
frobulochkina <konyakova.elena@gmail.com>
2022-02-20 12:22:12 +0300

    fixed method delete in Traccker

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -60,12 +60,10 @@ public class Tracker {
         int index = indexOf(id);
         boolean rsl = index != -1;
         if (rsl) {
-            items[index] = null;
             System.arraycopy(items, index, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
-            return true;
         }
-        return false;
+        return rsl;
     }
 }
