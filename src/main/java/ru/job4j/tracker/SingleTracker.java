50b57ef8b6675972f26c22feb3e0f9415352a3be
frobulochkina <konyakova.elena@gmail.com>
2022-03-07 18:49:10 +0300

    deleted methods in SingleTracker

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -13,28 +13,4 @@ public class SingleTracker {
         }
         return tracker;
     }
-
-    public Item add(Item item) {
-        return tracker.add(item);
-    }
-
-    public Item findById(int id) {
-        return tracker.findById(id);
-    }
-
-    public Item findByName(String key) {
-        return findByName(key);
-    }
-
-    public Item[] findAll() {
-        return tracker.findAll();
-    }
-
-    public boolean replace(int id, Item item) {
-        return tracker.replace(id, item);
-    }
-
-    public boolean delete(int id) {
-        return tracker.delete(id);
-    }
 }
