1d122ae11d49f77d2f9eb17761805a6c6da67de4
frobulochkina <konyakova.elena@gmail.com>
2022-03-26 13:06:25 +0300

    added search

---
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,22 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList<>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    public ArrayList<Person> find(String key) {
+        ArrayList<Person> result = new ArrayList<>();
+        for (int i = 0; i < persons.size(); i++) {
+            if (persons.get(i).getSurname().equals(key) || persons.get(i).getName().equals(key) ||
+                    persons.get(i).getPhone().equals(key) || persons.get(i).getAddress().equals(key)) {
+                result.add(persons.get(i));
+            }
+        }
+        return result;
+    }
+}
